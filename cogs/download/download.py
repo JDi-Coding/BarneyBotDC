fdcbe8d7b38f7cc3b5389df868440c98e44aaab7
JDi-Coding <Jasondiers@aol.com>
2024-10-21 17:57:49 +0200

    Refactored download_cog.py

---
+++ cogs/download/download.py
@@ -0,0 +1,59 @@
+import os
+from logging import raiseExceptions
+
+import yt_dlp
+from imageio.plugins.ffmpeg import download
+from jinja2.nodes import Import
+from sympy.codegen.ast import Raise
+
+import discord
+from discord import app_commands
+from discord.ext import commands
+import logging
+from config import settings
+logging.config.dictConfig(settings.LOGGING_CONFIG)
+logger = logging.getLogger('bot')
+
+class Download:
+    def __init__(self, input_url: str):
+        try:
+            self.url = None
+            self.info = None
+            self.filename = None
+            self.title = None
+            self.download_dir = "C:/Users/jason/Downloads/ytdownload"
+
+            if not os.path.exists(self.download_dir):
+                os.makedirs(self.download_dir)
+
+            self.ydl_opts = {
+                'format': 'bestvideo[ext=mp4]+bestaudio[ext=m4a]', #Beste Video und Audio Qualitaet
+                'outtmpl': f'{self.download_dir}/%(title)s.%(ext)s', # Pfad wo das Video gespeichert wird
+            }
+
+            self.Set_Url(input_url)
+            self.Download_from_Url()
+        except Exception as e:
+            raise e
+
+    #Setze die Url des Videos
+    def Set_Url(self, url: str):
+        try:
+            if url is not None and url != '':
+                self.url = url
+        except Exception as e:
+            #Leite den Fehler eine Ebene Weiter
+            raise e
+
+    #Downloade das Video
+    def Download_from_Url(self):
+        try:
+            with yt_dlp.YoutubeDL(self.ydl_opts) as ydl:
+                self.info = ydl.extract_info(self.url)
+                self.filename = ydl.prepare_filename(self.info)
+                self.title = self.info.get('title', None)
+        except Exception as e:
+            raise e
+
+    def __del__(self):
+        logger.info('Object Download destroyed')
---
+++ cogs/download_cog.py
@@ -0,0 +1,37 @@
+
+import discord
+from discord import app_commands
+from discord.ext import commands
+
+from config.config import PREMIUM_GUILD_IDS
+from config import settings
+
+
+
+from cogs.download.download import Download
+
+import logging
+logging.config.dictConfig(settings.LOGGING_CONFIG)
+logger = logging.getLogger('bot')
+
+
+class DownloadCog(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    DownloadGroup = app_commands.Group(name="download", description="download videos from the internet", guild_ids=PREMIUM_GUILD_IDS)
+
+    #Command zum Downloaden von YT Videos
+    @DownloadGroup.command(name="yt", description="downloads the specified youtube video")
+    async def download(self, interaction: discord.Interaction, url: str):
+        await interaction.response.defer(thinking=True, ephemeral=True)
+        try:
+            Download(url)
+            await interaction.edit_original_response(content="Video Heruntergeladen")
+        except Exception as e:
+            logger.error(f"Error: async def download: {e}")
+            await  interaction.edit_original_response(content=f"Fehler beim Herunterladen: {e}")
+
+
+async def setup(bot):
+    await bot.add_cog(DownloadCog(bot))
